2db884ca81611780775be9b7cc188646ace8aece
Juan Casanova Gago <casanovagagojuan@gmail.com>
2021-11-23 12:30:54 +0100

    Creacion y modificacion del primer prpoyecto del git EjerciciosProgramacionJava

---
+++ HolaMundoGit/src/holamundogit/HolaMundoGit.java
@@ -0,0 +1,7 @@
+package holamundogit;
+public class HolaMundoGit {
+    public static void main(String[] args) {
+        System.out.println("Primer ejercicio de el repositorio: EjerciciosProgramacionJava");
+    }
+    
+}
